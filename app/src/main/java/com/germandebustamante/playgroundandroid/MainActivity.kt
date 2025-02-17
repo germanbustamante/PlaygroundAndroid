c3bfac8491b39f523cdac6af72511ca18dbe144b
GermanBustamante <germanbustamante.trabajo@gmail.com>
2025-02-17 01:54:35 +0100

    Created Trusted time API example

--- app/src/main/java/com/germandebustamante/playgroundandroid/MainActivity.kt
+++ app/src/main/java/com/germandebustamante/playgroundandroid/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.germandebustamante.playgroundandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.time.TrustedTime
+import com.google.android.gms.time.TrustedTimeClient
 
 class MainActivity : AppCompatActivity() {
+
+    private var trustedTimeClient: TrustedTimeClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        TrustedTime.createClient(this).addOnSuccessListener {
+            trustedTimeClient = it
+        }.addOnFailureListener {
+            Log.e("MainActivity", "Failed to create TrustedTimeClient", it)
+        }
+
+        Log.i("MainActivity", "Current time in millis: ${getCurrentTimeInMillis()}")
     }
+
+    private fun getCurrentTimeInMillis(): Long =
+        trustedTimeClient?.computeCurrentUnixEpochMillis() ?: System.currentTimeMillis()
 }
